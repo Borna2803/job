1d0aec980d6d247d1471446ced7694bf86af60bb
Borna <borna.rt6@gmail.com>
2023-02-27 14:54:57 +0100

    web scrapper

--- task_2/hgspot_products.py
+++ task_2/hgspot_products.py
@@ -2,15 +2,17 @@ from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 
-def compare(product_link: str):
-    nabava_url      = 'https://www.nabava.net'
-    result          = requests.get(nabava_url+product_link)
-    soup            = bs(result.text, "html.parser")
-    offers          = soup.find_all("div", class_="offer")
-    i               = 1
-    shops           = []
-    hgspot_position = -1
-    position_before_us = 0
+def compare(product_link: str, hgspot_price: str, hgspot_des: str):
+    
+    result              = requests.get(product_link)
+    soup                = bs(result.text, "html.parser")
+    
+    product_name        = soup.find_all( "h1", class_="product-page__title" )[0].text
+    offers              = soup.find_all( "div", class_="offer" )
+    current_position    = 1
+    shops               = []
+    hgspot_position     = -1
+    position_before_us  = 0
     
     for offer in offers:
         shop_name   = offer.find( "div", class_="offer__store-link" ) ['data-event-label'] .split("-")[1].strip()
@@ -18,20 +20,22 @@ def compare(product_link: str):
         
         shops.append({      'name':     shop_name, 
                             'price':    shop_price, 
-                            'position': i   
+                            'position': current_position   
                     })
         
         if shop_name == 'HGSPOT' and hgspot_position == -1:
-            hgspot_position = i 
-            position_before_us = len(shops) 
+            hgspot_position = current_position
+            if len(shops) >= 1:
+                position_before_us = len(shops) 
              
-        i += 1
+        current_position += 1
 
     second_place_price  = 0
     third_place_price   = 0
     fourth_place_price  = 0
 
-    position_before_us  = 0
+    min=shops[0]['price']
+    max=shops[-1]['price']
     
     if len(shops) >= 4:
         fourth_place_price = shops[3]['price']
@@ -40,14 +44,26 @@ def compare(product_link: str):
     if len(shops) >= 2:
         second_place_price = shops[1]['price']
     
-    if position_before_us >= 1:
-        position_before_us = shops[position_before_us]['price']
-        
-    min=shops[0]['price']
-    max=shops[-1]['price']
-    
-    return [ hgspot_position, position_before_us, min, max, second_place_price, third_place_price, fourth_place_price]
+    price_before_us = 0
+    if position_before_us >= 2:
+        price_before_us = shops[position_before_us-2]['price']
 
+    
+    product_info={
+        "ime proizvoda":                    product_name,
+        "hgspot opis proizvoda":            hgspot_des,
+        "link proizvoda":                   product_link,
+        "hgspot pozicija":                  hgspot_position, 
+        "hgspot cijena":                    hgspot_price,
+        "cijena ponude prije nas":          price_before_us, 
+        "najjeftinija cijena proizvoda":    min, 
+        "najskuplja cijena proizvoda":      max, 
+        "prva cijena":                      min, 
+        "druga cijena":                     second_place_price,
+        "treca cijena":                     third_place_price, 
+        "cetvrta cijena":                   fourth_place_price, 
+    }
+    return product_info
 
 def main():
 
@@ -68,24 +84,12 @@ def main():
         for product in products:
             product_des         = product.find("div", class_="offer__name").text
             product_price       = product.find("div", class_="offer__price").text
-            product_link        = product.find( "a", class_="offer__buttons-other-offers" ) [ "href" ]
+            product_link        = 'https://www.nabava.net' + product.find( "a", class_="offer__buttons-other-offers" ) [ "href" ]
             
-            list = compare(product_link)
+            product_info = compare(product_link, product_price, product_des)
+
             
-            list_of_products.append({   
-                                    "ime proizvoda":                    product_des.split(",")[0], 
-                                    "hgspot opis proizvoda":            product_des, 
-                                    "link proizvoda":                   'https://www.nabava.net' + product_link, 
-                                    "hgspot cijena proizvoda":          product_price, 
-                                    "hgspot pozicija":                  list[0], 
-                                    "cijena ponude prije nas":          list[1],
-                                    "najjeftinija cijena proizvoda":    list[2], 
-                                    "najskuplja cijena proizvoda":      list[3], 
-                                    "prva cijena":                      list[0], 
-                                    "druga cijena":                     list[4],
-                                    "treca cijena":                     list[5], 
-                                    "cetvrta cijena":                   list[6], 
-                                    })
+            list_of_products.append(product_info)
 
             
     df = pd.DataFrame(list_of_products)
